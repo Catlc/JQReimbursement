af891218a02401e1458ce8dcce3e4aae9e431448
李承 <165205770@qq.com>
2018-07-05 17:40:15 +0800

    Initial Commit

---
+++ JQReimbursement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JQReimbursement
+//
+//  Created by 李承 on 2018/7/5.
+//  Copyright © 2018年 licheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JQReimbursement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JQReimbursement
+//
+//  Created by 李承 on 2018/7/5.
+//  Copyright © 2018年 licheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
